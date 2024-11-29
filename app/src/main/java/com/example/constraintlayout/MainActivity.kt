ff14b6b598001c2b11677a41208cc0b3af39fba9
Andravi <andravi75@gmail.com>
2024-11-29 19:21:59 -0300

    started project

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -10,53 +10,77 @@ import android.view.View
 import android.widget.EditText
 import java.util.*
 
-class MainActivity : AppCompatActivity() , TextWatcher, TextToSpeech.OnInitListener {
+class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
     private lateinit var tts: TextToSpeech
     private lateinit var edtConta: EditText
+    private lateinit var inputPeople: EditText
     private var ttsSucess: Boolean = false;
+
+    private var moneyToSplit: Double = 0.0
+    private var peapleNumber: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         edtConta = findViewById<EditText>(R.id.edtConta)
-        edtConta.addTextChangedListener(this)
+        inputPeople = findViewById<EditText>(R.id.inputPeople)
+
+
+
+        edtConta.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("PDM24","Antes de mudar")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("PDM24","Mudando")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d ("PDM24", "Depois de mudar")
+
+                val valor: Double
+
+                if(s.toString().isNotEmpty()) {
+                    moneyToSplit = s.toString().toDouble()
+                    Log.d("PDM24", "v: $moneyToSplit")
+                    //    edtConta.setText("9")
+                }
+            }
+        })
+
+        inputPeople.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("PDM24","People mudado")
+            }
+        })
+
+
         // Initialize TTS engine
         tts = TextToSpeech(this, this)
 
     }
 
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-       Log.d("PDM24","Antes de mudar")
 
-    }
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        Log.d("PDM24","Mudando")
-    }
-
-    override fun afterTextChanged(s: Editable?) {
-        Log.d ("PDM24", "Depois de mudar")
-
-        val valor: Double
-
-        if(s.toString().length>0) {
-             valor = s.toString().toDouble()
-            Log.d("PDM24", "v: " + valor)
-        //    edtConta.setText("9")
-        }
-    }
-
+// Fazer uma validação para não deixar colocar 0 Pessoas
     fun clickFalar(v: View){
+        Log.d ("teste", this.inputPeople.text.toString())
         if (tts.isSpeaking) {
             tts.stop()
         }
         if(ttsSucess) {
-            Log.d ("PDM23", tts.language.toString())
-            tts.speak("Oi Sumido", TextToSpeech.QUEUE_FLUSH, null, null)
+
+            tts.speak("Você tem $moneyToSplit Reais a ser pago por $peapleNumber Pessoas, que dá ${moneyToSplit/peapleNumber} reais para cada um.", TextToSpeech.QUEUE_FLUSH, null, null)
         }
-
-
-
-
     }
     override fun onDestroy() {
             // Release TTS engine resources
